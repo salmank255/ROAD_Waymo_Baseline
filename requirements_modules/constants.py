3473625c1f7bd36769a7cf0f6cf2df866a0578d2
Mihaela Catalina <c.mihaela.stoian@gmail.com>
2023-04-17 15:40:25 +0100

    changed constants to have 281 constraints

--- requirements_modules/constants.py
+++ requirements_modules/constants.py
@@ -3,7 +3,7 @@ NUM_LABELS = 41 #149
 BETA = 0.1
 
 CONSTRAINTS_PATH = "requirements_modules/constraints/constraints.txt"
-NUM_REQ = 243 #246
+NUM_REQ = 281 #243
 
 # CONSTRAINTS_PATH = "requirements_modules/constraints/constraints_small.txt"
 # NUM_REQ = 10 #246
