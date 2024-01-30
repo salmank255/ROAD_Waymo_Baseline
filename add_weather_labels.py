443b8ea5bd9f6cc5ad4e165fea5540f8d8f79181
Salman Khan <salmank255@gmail.com>
2024-01-30 19:15:38 +0000

    added weather labels

---
+++ add_weather_labels.py
@@ -0,0 +1,51 @@
+
+import json
+import numpy as np
+import csv
+
+def find_in_list_of_list(mylist, char):
+    for sub_list in mylist:
+        if char in sub_list:
+            return (mylist.index(sub_list), sub_list.index(char))
+    raise ValueError("'{char}' is not in list".format(char = char))
+
+
+road_trainval_path = '../road_waymo/road_waymo_trainval_v1.0.json'
+with open(road_trainval_path,'r') as fff:
+    road_json = json.load(fff)
+
+file = open('weather_trainval.csv')
+csvreader = csv.reader(file)
+rows = []
+for row in csvreader:
+    rows.append(row)
+
+
+for vid in road_json['db']:
+    vid_ind = find_in_list_of_list(rows, vid)[0]
+    weather = rows[vid_ind][1]
+    road_json['db'][vid]['weather'] = weather
+
+with open('../road_waymo/road_waymo_trainval_v1.1.json', 'w') as f:
+    json.dump(road_json, f)
+
+
+
+
+road_test_path = '../road_waymo/road_waymo_test_v1.0.json'
+with open(road_test_path,'r') as fff:
+    road_json = json.load(fff)
+
+file = open('weather_test.csv')
+csvreader = csv.reader(file)
+rows = []
+for row in csvreader:
+    rows.append(row)
+
+for vid in road_json['db']:
+    vid_ind = find_in_list_of_list(rows, vid)[0]
+    weather = rows[vid_ind][1]
+    road_json['db'][vid]['weather'] = weather
+
+with open('../road_waymo/road_waymo_test_v1.1.json', 'w') as f:
+    json.dump(road_json, f)
